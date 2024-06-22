f416070cd1519759d3fba233aa98470c0f6366d2
SylvainJnn <sylvainjannin@gmail.com>
2024-06-22 15:03:01 +0900

    update comment

--- Visual Odometry/plot_data.py
+++ Visual Odometry/plot_data.py
@@ -30,7 +30,7 @@ def read_data(filename):
         for line in file:
             # sepearent element of the line 
             elements = line.split()
-            # get 3rd and 9th element --> (x, z)
+            # get 4rd and 12th element --> (x, z)
             x = float(elements[3])  # Le 4ème élément (index 3)
             z = float(elements[11])  # Le 12ème élément (index 11)
             data.append([x, z])
@@ -42,9 +42,9 @@ def read_tuto_data(filename):
         for line in file:
             # sepearent element of the line 
             elements = line.split()
-            # get 4rd and 11th element --> (x, z)
-            x = float(elements[0])  # Le 4ème élément (index 3)
-            z = float(elements[1])  # Le 12ème élément (index 11)
+            # get 1st and 2d element --> (x, z)
+            x = float(elements[0])  
+            z = float(elements[1])  
             data.append([x, z])
     return data
 
@@ -55,17 +55,3 @@ def read_data_pandas(filename):
     data = df.iloc[:, [4, 11]].values.tolist()
     return data
 
-
-if __name__ == "__main__":
-    my_data_seq1 = data("my_data_seq1", 
-                         read_data("poses/my_poses_seq1.txt")) 
-    ground_truth_data_seq1 = data("ground_truth_data_seq1",
-                                   read_data("example/KITTI_sequence_1/poses.txt"))
-    data_tuto_seq1 = data("data_tuto_seq1",
-                           read_tuto_data("poses/tuto_poses_seq1.txt"))
-    
-    data = [my_data_seq1,
-            ground_truth_data_seq1,
-            data_tuto_seq1]
-    
-    plot_data(data)
