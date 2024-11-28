d06297503043547b118d125b1b31544ed8e1b785
SylvainJnn <sylvainjannin@gmail.com>
2024-11-28 13:44:53 +0900

    update plot data

--- Visual Odometry/plot_data.py
+++ Visual Odometry/plot_data.py
@@ -1,25 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 
 class data:
-    def __init__(self, new_name, new_pointclouds):
+    def __init__(self, new_name, new_poses):
         self.name = new_name
-        self.pointclouds = new_pointclouds
+        self.poses = new_poses
 
 
-def plot_data(data):
+def plot_data(data, line=True, count=True):
     for d in data:
-        dx = [points[0] for points in d.pointclouds]
-        dz = [points[1] for points in d.pointclouds]
-        plt.scatter(dx, dz, label=d.name)
+        dx = [points[0] for points in d.poses]
+        dz = [points[1] for points in d.poses]
+
+        if(line):
+            plt.plot(dx, dz, label=d.name)
+        else:
+            plt.scatter(dx, dz, label=d.name)
+
+        # Ajouter des numéros pour chaque point
+        if(count):
+            if(d.name != "Ground truth data seq2"):
+                for i, (x, z) in enumerate(zip(dx, dz)):
+                    plt.text(x, z, str(i+1), fontsize=9, ha='right', va='bottom')  # Affichage du numéro (i+1)
+
 
     plt.xlabel('X')
     plt.ylabel('Z')
-    plt.title('Visual Odometetry point clouds')
+    plt.title('Visual Odometetry path poses')
     plt.legend()
     plt.grid(True)
+
+    # set 1:1 scalre size
+    # plt.gca().set_aspect('equal', adjustable='box')
     
     plt.show()
 
@@ -36,22 +49,23 @@ def read_data(filename):
             data.append([x, z])
     return data
 
-def read_tuto_data(filename):
-    data = []
-    with open(filename, 'r') as file:
-        for line in file:
-            # sepearent element of the line 
-            elements = line.split()
-            # get 1st and 2d element --> (x, z)
-            x = float(elements[0])  
-            z = float(elements[1])  
-            data.append([x, z])
-    return data
+if __name__ == "__main__":
+   
+    ground_truth_seq1 = data('Ground truth data seq1', 
+                             read_data("example/KITTI_sequence_1/poses.txt"))
+    
+    ground_truth_seq2 = data('Ground truth data seq2', 
+                             read_data("example/KITTI_sequence_2/poses.txt"))
 
-def read_data_pandas(filename):
-    # read file
-    df = pd.read_csv(filename, delim_whitespace=True, header=None)
-    # get x and z
-    data = df.iloc[:, [4, 11]].values.tolist()
-    return data
-
+    # example_data_2D_2D = data('Example data', 
+    #                     read_data("poses/seq2_2D_2D.txt"))
+    
+    # example_data_23_2D = data('Example data', 
+    #                     read_data("poses/mono_3D_2D_my_poses_seq2.txt"))
+    
+    # plot_data([
+    #     ground_truth_seq1,
+    #     example_data_2D_2D
+    #     ],
+    #     line=False,
+    #     count=True)
